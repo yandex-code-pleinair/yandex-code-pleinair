2a43eb0e096ed9dbbc8f46f214988deecf0cf71b
Sergey Berezhnoy <veged@yandex-team.ru>
2024-09-21 03:52:26 +0300

    screenshots: wait for 150 frames

--- screenshots.spec.js
+++ screenshots.spec.js
@@ -3,7 +3,10 @@ import test from '@playwright/test'
 require('./plants/plants.json').slice(0, 149).forEach(plant => {
   test('make screenshot: ' + plant, async ({ page }) => {
     await page.setViewportSize({ width: 450, height: 800 })
+    // page.on('console', msg => console.log(msg.text()))
     await page.goto(`http://${plant}.localhost:4000/plants/${plant}/index.html`)
+    await page.evaluate(() => window.frameRate(80))
+    await page.waitForFunction(() => window.frameCount === 150)
     await page.locator('canvas').screenshot({ path: `./screenshots/${plant}.png` })
   })
 })
