37cd7290efd4b16ce5da1942b8b395457433b6b2
Sergey Berezhnoy <veged@yandex-team.ru>
2024-09-21 15:18:57 +0300

    first bunch of screenshots

--- screenshots.spec.js
+++ screenshots.spec.js
@@ -1,12 +1,12 @@
 import test from '@playwright/test'
 
-require('./plants/plants.json').slice(0, 149).forEach(plant => {
+require('./plants/changed.json').slice(0, 149).forEach(plant => {
   test('make screenshot: ' + plant, async ({ page }) => {
     await page.setViewportSize({ width: 450, height: 800 })
     // page.on('console', msg => console.log(msg.text()))
     await page.goto(`http://${plant}.localhost:4000/plants/${plant}/index.html`)
     await page.evaluate(() => window.frameRate(80))
-    await page.waitForFunction(() => window.frameCount === 150)
+    await page.waitForFunction(() => !window.isLooping() || window.frameCount === 1000)
     await page.locator('canvas').screenshot({ path: `./screenshots/${plant}.png` })
   })
 })
